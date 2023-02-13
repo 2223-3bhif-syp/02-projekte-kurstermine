1d705a6099e139b6e03039408c3eac3d2a186967
ICQun <jln.schludermann@gmail.com>
2023-02-13 09:59:46 +0100

    23kurs-16 Add initial implementation of Entity classes

---
+++ CourseSchedule/src/main/java/at/htl/courseschedule/entity/Participant.java
@@ -0,0 +1,76 @@
+package at.htl.courseschedule.entity;
+
+public class Participant {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private int yearOfBirth;
+    private String phoneNr;
+    String email;
+
+    //region Constructors
+    public Participant() {
+    }
+
+    public Participant(String firstName, String lastName, int yearOfBirth, String phoneNr, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        this.phoneNr = phoneNr;
+        this.email = email;
+    }
+    //endregion
+
+    //region Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getPhoneNr() {
+        return phoneNr;
+    }
+
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+    //endregion
+    @Override
+    public String toString() {
+        return "Participant{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                ", phoneNr='" + phoneNr + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
