7ef75f4a14a77a5155bce7fa6f75f5776bb526f6
ICQun <jln.schludermann@gmail.com>
2023-02-26 11:52:16 +0100

    Renamed Tables and Attributes based on suggested scheme

--- CourseSchedule/src/main/java/at/htl/courseschedule/controller/Main.java
+++
@@ -1,9 +0,0 @@
-package at.htl.courseschedule.controller;
-
-import at.htl.courseschedule.database.SqlRunner;
-
-public class Main {
-    public static void main(String[] args) {
-        SqlRunner.dropAndCreateTablesWithExampleData();
-    }
-}
