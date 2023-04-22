fe9a9f66d47f6adab780e960b6fd69acd7fc6bea
ICQun <jln.schludermann@gmail.com>
2023-04-22 16:59:20 +0200

    23-kurs-18 To Verify Improve Unit tests for the Participant class

--- CourseSchedule/src/test/java/at/htl/courseschedule/entity/ParticipantTest.java
+++ CourseSchedule/src/test/java/at/htl/courseschedule/entity/ParticipantTest.java
@@ -2,9 +2,8 @@ package at.htl.courseschedule.entity;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.assertj.db.api.Assertions.assertThat;
 import org.assertj.db.type.DateValue;
 import org.assertj.db.type.Source;
 import org.assertj.db.type.Table;
@@ -22,13 +21,95 @@ class ParticipantTest {
     }
 
     @Test
-    void testToString() {
+    void test_ToString_AfterNormalCtor_ShouldResultEnteredValuesAndIdNull() {
         // arrange
-        Participant participant = new Participant("firstName", "lastName", 2000, "+43 6704070789", "lastName@gmail.com");
+        Participant participant = new Participant("firstName", "lastName", 2000,
+                "+43 6704070789", "lastName@gmail.com");
 
-        // modify
+        // act
 
-        // test
-        assertEquals("Participant{id=null, firstName='firstName', lastName='lastName', yearOfBirth=2000, phoneNr='+43 6704070789', email='lastName@gmail.com'}", participant.toString());
+        // assert
+        assertThat(participant.toString())
+                .isEqualTo("Participant{id=null, firstName='firstName', lastName='lastName', yearOfBirth=2000, phoneNr='+43 6704070789', email='lastName@gmail.com'}");
+    }
+
+    @Test
+    void test_Getters_AfterNormalCtor_ShouldResultEnteredValuesAndIdNull() {
+        // arrange
+        Participant participant = new Participant("firstName", "lastName", 2000,
+                "+43 6704070789", "lastName@gmail.com");
+
+        // act
+
+        // assert
+        assertThat(participant.getId()).isNull();
+        assertThat(participant.getEmail()).isEqualTo("lastName@gmail.com");
+        assertThat(participant.getFirstName()).isEqualTo("firstName");
+        assertThat(participant.getLastName()).isEqualTo("lastName");
+        assertThat(participant.getYearOfBirth()).isEqualTo(2000);
+        assertThat(participant.getPhoneNr()).isEqualTo("+43 6704070789");
+    }
+
+    @Test
+    void test_Getters_AfterDefaultCtor_ShouldResultInDefaultValues() {
+        // arrange
+        Participant participant = new Participant();
+
+        // act
+
+        // assert
+        assertThat(participant.getId()).isNull();
+        assertThat(participant.getEmail()).isNull();
+        assertThat(participant.getFirstName()).isNull();
+        assertThat(participant.getLastName()).isNull();
+        assertThat(participant.getYearOfBirth()).isEqualTo(0);
+        assertThat(participant.getPhoneNr()).isNull();
+    }
+
+    @Test
+    void test_Setters_SimpleChangesOfDefaultValues_ShouldResultInNewValues() {
+        // arrange
+        Participant participant = new Participant();
+
+        // act
+        participant.setId(0L);
+        participant.setEmail("something@example.com");
+        participant.setFirstName("Hans");
+        participant.setLastName("Müller");
+        participant.setPhoneNr("+43 681 2202234");
+        participant.setYearOfBirth(2010);
+
+
+        // assert
+        assertThat(participant.getId()).isEqualTo(0L);
+        assertThat(participant.getEmail()).isEqualTo("something@example.com");
+        assertThat(participant.getFirstName()).isEqualTo("Hans");
+        assertThat(participant.getLastName()).isEqualTo("Müller");
+        assertThat(participant.getYearOfBirth()).isEqualTo(2010);
+        assertThat(participant.getPhoneNr()).isEqualTo("+43 681 2202234");
+    }
+
+    @Test
+    void test_Setters_SimpleChangesOfCtorSetValues_ShouldResultInNewValues() {
+        // arrange
+        Participant participant = new Participant("Anita", "Huber", 1999,
+                "+43 681 0304877", "anitaHuber@gmail.com");
+
+        // act
+        participant.setId(23L);
+        participant.setEmail("iexist@yahoomail.com");
+        participant.setFirstName("Christoph");
+        participant.setLastName("Ilming");
+        participant.setPhoneNr("+43 722 23409978");
+        participant.setYearOfBirth(2011);
+
+
+        // assert
+        assertThat(participant.getId()).isEqualTo(23L);
+        assertThat(participant.getEmail()).isEqualTo("iexist@yahoomail.com");
+        assertThat(participant.getFirstName()).isEqualTo("Christoph");
+        assertThat(participant.getLastName()).isEqualTo("Ilming");
+        assertThat(participant.getYearOfBirth()).isEqualTo(2011);
+        assertThat(participant.getPhoneNr()).isEqualTo("+43 722 23409978");
     }
 }
